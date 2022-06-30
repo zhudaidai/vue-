1d5d495abf36a4ed5519c1113fe44373556c0436
zhudaidai <1010462584@qq.com>
2022-06-30 19:25:29 +0800

    vue第三天作业

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+/* 覆盖webpack的配置 */
+module.exports = {
+  devServer: { // 自定义服务配置
+    open: true, // 自动打开浏览器
+    port: 3000
+  }
+}
