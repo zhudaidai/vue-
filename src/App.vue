55b385b47e3422b6d0db8bfb2246a7f5b5c087b5
zhudaidai <1010462584@qq.com>
2022-06-30 23:59:33 +0800

    案例-品牌管理

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,132 @@
 <template>
   <div id="app">
-   
+    <div class="container">
+      <!-- 顶部框模块 -->
+      <div class="form-group">
+        <div class="input-group">
+          <h4>品牌管理</h4>
+        </div>
+      </div>
+
+      <!-- 数据表格 -->
+      <table class="table table-bordered table-hover mt-2">
+        <thead>
+          <tr>
+            <th>编号</th>
+            <th>资产名称</th>
+            <th>价格</th>
+            <th>创建时间</th>
+            <th>操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="obj in list" :key="obj.id">
+        <td>{{ obj.id }}</td>
+        <td>{{ obj.name }}</td>
+
+            <!-- 如果价格超过100，就有red这个类 -->
+            <td :class="{red: obj.price > 100}">{{ obj.price }}</td>
+        <td>{{ obj.time }}</td>
+        <td><a href="#" @click="delFn(obj.id)">删除</a></td>
+          </tr>
+          <!-- <tr style="background-color: #EEE">
+              <td>统计:</td>
+              <td colspan="2">总价钱为: 0</td>
+              <td colspan="2">平均价: 0</td>
+          </tr> -->
+        </tbody>
+          <!-- 
+        <tfoot >
+          <tr>
+            <td colspan="5" style="text-align: center">暂无数据</td>
+          </tr>
+        </tfoot>
+            -->
+      </table>
+
+      <!-- 添加资产 -->
+      <form class="form-inline" style="display:flex;">
+        <div class="form-group">
+          <div class="input-group">
+            <input
+       type="text"
+       class="form-control"
+       placeholder="资产名称"
+       v-model="name"
+       />
+          </div>
+        </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <div class="form-group">
+          <div class="input-group">
+           <input
+       type="text"
+       class="form-control"
+       placeholder="价格"
+       v-model.number="price"
+       />
+          </div>
+        </div>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <!-- 阻止表单提交 -->
+      <button class="btn btn-primary" @click.prevent="addFn">添加资产</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-
-
+import "bootstrap/dist/css/bootstrap.css"
+// 1. 明确需求
+// 2. 标签+样式+默认数据
+// 3. 下载bootstrap, main.js引入bootstrap.css
+// 4. 把list数组 - 铺设表格
+// 5. 修改价格颜色
 export default {
-  name: 'App',
-  
+ data() {
+    return {
+      list: [
+        { id: 100, name: "外套", price: 199, time: new Date('2010-08-12')},
+        { id: 101, name: "裤子", price: 34, time: new Date('2013-09-01') },
+        { id: 102, name: "鞋", price: 25.4, time: new Date('2018-11-22') },
+        { id: 103, name: "头发", price: 19900, time: new Date('2020-12-12') }
+      ],
+       name: '',
+    price: 0
+    };
+  },
+  methods: {
+     delFn(id){
+      // 通过id找到这条数据在数组中下标
+      let index = this.list.findIndex(obj => obj.id === id)
+      this.list.splice(index, 1)
+    },
+  addFn () {
+       // 5. 判断是否为空
+      if (this.name.trim().length == 0 || this.price == 0) {
+        this.name = ''
+        this.price = 0
+        return alert("请输入完整的信息");
+      }
+      // 3. 把值以对象形式-插入list
+      this.list.push({
+        // 当前数组最后一个对象的id+1作为新对象id值
+        id: this.list[this.list.length - 1].id+1,
+        name: this.name,
+        price: this.price,
+        time: new Date()
+      })
+      // 清空输入的值
+      this.name = ''
+      this.price = 0
+    }
 }
+
+};
 </script>
 
-<style>
-
+<style >
+.red{
+  color: red;
+}
 </style>
