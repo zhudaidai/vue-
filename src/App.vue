f8f465bd14301114224908c49c0259548b18a9d4
zhudaidai <1010462584@qq.com>
2022-06-30 20:38:01 +0800

    vue基础 v-for更新监测

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-   
+   <ul>
+     <li v-for="(val,index) in arr" :key="index">{{val}}</li>
+   </ul>
+   <button @click="revBtn">数组翻转</button>
+    <button @click="sliceBtn">截取前三个</button>
+     <button @click="updateBtn">更新第一个元素值</button>
   </div>
 </template>
 
@@ -8,7 +13,23 @@
 
 
 export default {
-  name: 'App',
+  data () {
+    return {
+      arr :[5,6,7,5,7,1]
+    }
+  },
+  methods: {
+    revBtn(){
+      this.arr.reverse()
+    },
+    sliceBtn(){
+      let newArr = this.arr.slice(0,3)
+      this.arr = newArr
+    },
+    updateBtn () {
+      this.$set(this.arr,0,1000)
+    }
+  }
   
 }
 </script>
