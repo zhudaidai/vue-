a2871842218284c8ea9a35695beab07244be32fc
zhudaidai <1010462584@qq.com>
2022-06-30 21:01:01 +0800

    案例练习

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-   
+    <ul>
+            <li :class="{'active': index === currentIndex}" v-for="(item,index) in navs" :key="index" 												@click="changeFn(index)">大学起点</li>
+        </ul>
+
+    
   </div>
 </template>
 
@@ -8,11 +12,39 @@
 
 
 export default {
-  name: 'App',
-  
+  data() {
+        return {
+            navs: ['大学起点', '高中起点', '初中起点', '小学起点'],
+            currentIndex: 0
+        };
+    },
+    methods: {
+        changeFn (index) {
+            this.currentIndex = index;
+        }
+    }
 }
 </script>
 
 <style>
-
+ul {
+    list-style: none;
+    border-radius: 10px;
+    width: 400px;
+    overflow: hidden;
+    padding: 0;
+  }
+  ul li {
+    float: left;
+    width: 100px;
+    height: 40px;
+    background-color: #ccc;
+    color: #fff;
+    text-align: center;
+    line-height: 40px;
+    cursor: pointer;
+  }
+  li.active {
+    background-color: blue;
+  }
 </style>
